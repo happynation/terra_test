3197e0f951ef7c975c644c94c21bd1219b49dc2c
vovatran1993 <vovatran1993@gmail.com>
2019-12-08 11:02:35 -0600

    added priv

--- subnet.tf
+++ subnet.tf
@@ -3,7 +3,19 @@ resource "aws_subnet" "public" {
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "${var.Name}"
+    Name = "${var.Name}.public"
+    Env  = "${var.Env}"
+    Created_by = "${var.Created_by}"
+    Dept = "${var.Dept}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = "aws_vpc.dev.id"
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "${var.Name}.private"
     Env  = "${var.Env}"
     Created_by = "${var.Created_by}"
     Dept = "${var.Dept}"
